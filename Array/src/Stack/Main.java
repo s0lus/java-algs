ba052b75b33bab40a08bf9877f50862c22545236
s0lus <mashenkoalex@gmail.com>
2019-02-25 21:00:49 +0300

    [Array][Stack] Array implementation of stack

---
+++ Array/src/Stack/Main.java
@@ -0,0 +1,26 @@
+package Stack;
+
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>(4);
+
+        String[] str = "to be or not to - be - - that - - - is".split(" ");
+
+        for (String s : str) {
+            if (!s.equals("-")) {
+                stack.push(s);
+            } else if (!stack.isEmpty()) {
+                System.out.println(stack.pop());
+            }
+        }
+
+        System.out.println("In stack " + stack.size());
+
+        Iterator<String> it = stack.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
--- Array/src/Stack/Stack.java
+++ Array/src/Stack/Stack.java
@@ -1,7 +1,64 @@
 package Stack;
 
-public class Stack {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Iterator;
+
+public class Stack<Item> {
+    private Item[] elements;
+    private int size;
+
+    public Stack(int cap) {
+        elements = (Item[]) new Object[cap];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void push(Item item) {
+        if (size == elements.length) {
+            resize(2 * elements.length);
+        }
+
+        elements[size++] = item;
+    }
+
+    public Item pop() {
+        Item item = elements[--size];
+        elements[size] = null;
+
+        if (size > 0 && size == elements.length / 4) {
+            resize(elements.length / 2);
+        }
+
+        return item;
+    }
+
+    public void resize(int max) {
+        Item[] temp = (Item[]) new Object[max];
+        for (int i = 0; i < size; i++) {
+            temp[i] = elements[i];
+        }
+
+        elements = temp;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+
+    private class ReverseArrayIterator implements Iterator<Item> {
+        @Override
+        public boolean hasNext() {
+            return size > 0;
+        }
+
+        @Override
+        public Item next() {
+            return elements[--size];
+        }
     }
 }
