d99d97eecbc382516e43e20bdca442ad06779ceb
s0lus <mashenkoalex@gmail.com>
2019-03-12 19:43:01 +0300

    [DynamicCoupling][QuickFind] Init commit for quick find impl

---
+++ src/dynamic_coupling/quick_find/Main.java
@@ -0,0 +1,28 @@
+package dynamic_coupling.quick_find;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int N = 10;
+        UF uf = new UF(N);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (!scanner.hasNext("done")) {
+            String[] input = scanner.next().split(" ");
+
+            int p = Integer.parseInt(input[0]);
+            int q = Integer.parseInt(input[1]);
+
+            if (uf.connected(p, q))
+                continue;
+
+            uf.union(p, q);
+
+            System.out.println(p + " " + q);
+        }
+
+        System.out.println("Count of the components: " + uf.count());
+    }
+}
---
+++ src/dynamic_coupling/quick_find/UF.java
@@ -0,0 +1,38 @@
+package dynamic_coupling.quick_find;
+
+public class UF {
+    // Base data structure
+    private int[] id;
+
+    // Count of components
+    private int count;
+
+    // Init N nodes
+    public UF(int N) {
+        count = N;
+
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    // Returns true, if p and q belong to one component
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    // Finding ID of the component for p
+    public int find(int p) {
+
+    }
+
+    // Adding connection between p and q
+    public void union(int p, int q) {
+
+    }
+}
