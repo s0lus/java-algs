dc0a59c2136028b8ed62bf4bf344bc7e9a50df36
s0lus <mashenkoalex@gmail.com>
2019-03-18 20:05:05 +0300

    [Sorting] Init template for sorting algs

---
+++ src/sorting/Base.java
@@ -0,0 +1,33 @@
+package sorting;
+
+abstract public class Base {
+    abstract public void sort(Comparable[] a);
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exchange(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return  false;
+            }
+        }
+
+        return true;
+    }
+}
