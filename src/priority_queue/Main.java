109144c9df8e447b8e4cb4506ca61772359d80dd
s0lus <mashenkoalex@gmail.com>
2019-04-15 19:23:15 +0300

    [Sorting][QueuePriority] API interface for queue with priority

---
+++ src/priority_queue/Main.java
@@ -0,0 +1,7 @@
+package priority_queue;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/priority_queue/MaxPQ.java
@@ -0,0 +1,36 @@
+package priority_queue;
+
+public class MaxPQ<Key extends Comparable<Key>> {
+
+    public MaxPQ() {
+
+    }
+
+    public MaxPQ(int max) {
+
+    }
+
+    public MaxPQ(Key[] a) {
+
+    }
+
+    void insert(Key v) {
+
+    }
+
+    Key max() {
+
+    }
+
+    Key delMax() {
+
+    }
+
+    boolean isEmpty() {
+
+    }
+
+    int size() {
+
+    }
+}
