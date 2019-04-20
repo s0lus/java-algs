a0a511c167b72cf893fe6c211c0331769f57fcce
s0lus <mashenkoalex@gmail.com>
2019-04-20 21:00:05 +0300

    [Sorting][QueuePriority] Impl MaxPQ(int max), isEmpty, delMax, insert

--- src/priority_queue/MaxPQ.java
+++ src/priority_queue/MaxPQ.java
@@ -1,13 +1,18 @@
 package priority_queue;
 
 public class MaxPQ<Key extends Comparable<Key>> {
+    // Binary heap
+    private Key pq[];
+
+    // pq[1..size], pq[0] - not using
+    private int size = 0;
 
     public MaxPQ() {
 
     }
 
     public MaxPQ(int max) {
-
+        pq = (Key[]) new Comparable[max + 1];
     }
 
     public MaxPQ(Key[] a) {
@@ -15,7 +20,8 @@ public class MaxPQ<Key extends Comparable<Key>> {
     }
 
     void insert(Key v) {
-
+        pq[++size] = v;
+        swim(size);
     }
 
     Key max() {
@@ -23,10 +29,18 @@ public class MaxPQ<Key extends Comparable<Key>> {
     }
 
     Key delMax() {
+        // Max key from the root
+        Key max = pq[1];
+        // Exchnage with the latest element
+        exchange(1, size--);
+        pq[size + 1] = null;
+        sink(1);
 
+        return max;
     }
 
     boolean isEmpty() {
+        return size == 0;
     }
 
     int size() {
