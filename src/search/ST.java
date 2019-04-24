507a036b095fd25a2888da3e0f4a7cd68c2a5ba5
s0lus <mashenkoalex@gmail.com>
2019-04-24 19:57:46 +0300

    [Search][SymbolTable] API interface

---
+++ src/search/ST.java
@@ -0,0 +1,17 @@
+package search;
+
+interface ST<Key, Value> {
+    void put(Key key, Value value);
+
+    Value get(Key key);
+
+    void delete(Key key);
+
+    boolean contains(Key key);
+
+    boolean isEmpty();
+
+    int size();
+
+    Iterable<Key> keys();
+}
