abdb75c7991ac3cb74b1312aaaa0ebcd2c0eeeb6
s0lus <mashenkoalex@gmail.com>
2019-05-09 21:06:52 +0300

    [Search][UnorderedLinkedList] Test client

--- src/search/Main.java
+++ src/search/Main.java
@@ -1,7 +1,20 @@
 package search;
 
+import search.sequental_search.UnorderedLinkedList;
+
 public class Main {
     public static void main(String[] args) {
+        String[] array = new String[]{"s", "e", "a", "r", "c", "h", "e", "x", "a", "m", "p", "l", "e"};
 
+        UnorderedLinkedList unorderedLinkedList = new UnorderedLinkedList();
+
+        for (int i = 0; i < array.length; i++) {
+            unorderedLinkedList.put(array[i], i);
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ' ');
+            System.out.println(unorderedLinkedList.get(array[i]));
+        }
     }
 }
