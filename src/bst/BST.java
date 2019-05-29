864b95dcb5268b881108916e4ba0845f402a96d6
s0lus <mashenkoalex@gmail.com>
2019-05-29 18:59:31 +0300

    [Search][BST] Impl get/put

--- src/bst/BST.java
+++ src/bst/BST.java
@@ -36,12 +36,51 @@ public class BST<Key extends Comparable<Key>, Value> {
             return x.N;
         }
 
-        public Value get(Key key) {
-            // TODO:
+        // Returns value related with key in sub-tree with root x.
+        // Returns null if key in sub-tree with root x doesn't exists.
+        private Value get(Node x, Key key) {
+            if (x == null) {
+                return null;
+            }
+
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0) {
+                return get(x.left, key);
+            } else if (cmp > 0) {
+                return get(x.right, key);
+            } else {
+                return x.value;
+            }
         }
 
+        private Node put(Node x, Key key, Value value) {
+            // If key exists in sub-tree with root x, then change him value to value.
+            // Else add to sub-tree new node with key (key) and value (value).
+            if (x == null) {
+                return new Node(key, value, 1);
+            }
+
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0) {
+                x.left = put(x.left, key, value);
+            } else if (cmp > 0) {
+                x.right = put(x.right, key, value);
+            } else {
+                x.value = value;
+            }
+
+            x.N = size(x.left) + size(x.right) + 1;
+
+            return x;
+        }
+
+        public Value get(Key key) {
+            return get(root, key);
+        }
+
+        // Key finding. If exists - change value, else increment tree
         public void put(Key key, Value value) {
-            // TODO:
+            root = put(root, key, value);
         }
 
         // TODO: min/max/floor/ceiling/select/rank/delete/deleteMin/deleteMax/keys
