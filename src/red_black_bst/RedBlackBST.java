23e06eb3e8641116df6f91b75849687fd1fa9262
s0lus <mashenkoalex@gmail.com>
2019-06-24 19:36:23 +0300

    [Search][RedBlackBST] rotateLeft/rotateRight

--- src/red_black_bst/RedBlackBST.java
+++ src/red_black_bst/RedBlackBST.java
@@ -40,6 +40,30 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
             return x.color = RED;
         }
 
+        Node rotateLeft(Node h) {
+            Node x = h.right;
+            h.right = x.left;
+            x.left = h;
+            x.color = h.color;
+            h.color = RED;
+            x.N = h.N;
+            h.N = 1 + size(h.left) + size(h.right);
+
+            return x;
+        }
+
+        Node rotateRight(Node h) {
+            Node x = h.left;
+            h.left = x.right;
+            x.right = h;
+            x.color = h.color;
+            h.color = RED;
+            x.N = h.N;
+            h.N = 1 + size(h.left) + size(h.right);
+
+            return x;
+        }
+
         public int size() {
             return size(root);
         }
