be80eec15a2bb51a691dfadb5f1ccfab3685a874
s0lus <mashenkoalex@gmail.com>
2019-06-24 19:33:47 +0300

    [Search][RedBlackBST] Minimum setup for red black bst

--- src/red_black_bst/RedBlackBST.java
+++ src/red_black_bst/RedBlackBST.java
@@ -1,4 +1,55 @@
 package red_black_bst;
 
+import array.queue.Queue;
+
 public class RedBlackBST<Key extends Comparable<Key>, Value> {
+    // root
+    private Node root;
+
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private class Node {
+        private Key key;
+
+        private Value value;
+
+        // Ref to sub-tree
+        private Node left;
+
+        // Ref to sub-tree
+        private Node right;
+
+        // Count of nodes in sub-tree with root;
+        private int N;
+
+        private boolean color;
+
+        public Node(Key key, Value value, int N, boolean color) {
+            this.key = key;
+            this.value = value;
+            this.N = N;
+            this.color = color;
+        }
+
+        private boolean isRed(Node x) {
+            if (x == null) {
+                return false;
+            }
+
+            return x.color = RED;
+        }
+
+        public int size() {
+            return size(root);
+        }
+
+        private int size(Node x) {
+            if (x == null) {
+                return 0;
+            }
+
+            return x.N;
+        }
+    }
 }
